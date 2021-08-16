d943343d1b84736398840c815cef6a7e7d7cb7f9
dnsv <dejan.svetec@protonmail.com>
2021-08-16 23:25:25 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const core = require('@actions/core');
+const Clubhouse = require('clubhouse-lib');
+
+async function isStateEmpty(client, stateId) {
+    const result = await client.searchStories(`state:${stateId}`, 1);
+
+    return result.total == 0;
+}
+
+async function createIteration(client) {
+    const today = new Date().toISOString().slice(0, 10);
+    const params = {
+        'name': core.getInput('name'),
+        'description': core.getInput('description') || '',
+        'start_date': today,
+        'end_date': today
+    };
+
+    const previousIterations = await client.listIterations();
+
+    if (previousIterations.length) {
+        previousIterations.sort(function (a, b) {
+            return new Date(b.end_date).getTime() - new Date(a.end_date).getTime();
+        });
+
+        params['start_date'] = previousIterations[0]['end_date'];
+    }
+
+    const iteration = await client.createIteration(params);
+
+    core.setOutput('iteration-created', true);
+    core.setOutput('url', iteration.app_url);
+    core.info(`Created iteration ${iteration.name}.`);
+
+    return iteration;
+}
+
+async function assignStoriesToIteration(client, iteration, stateId) {
+    client.searchStories(`state:${stateId}`)
+        .then(result => {
+            if (!result.total) {
+                core.info('No stories found in the given workflow state.');
+                return;
+            }
+
+            for ({ id } of result.data) {
+                client.updateStory(id, { 'iteration_id': iteration.id });
+            }
+
+            if (!result.fetchNext) {
+                core.info(`Assigned ${result.total} stories to the iteration ${iteration.name}.`)
+                return;
+            }
+
+            result.fetchNext()
+                .then(processResult);
+        });
+}
+
+async function run() {
+    try {
+        const token = core.getInput('token');
+        const client = Clubhouse.create(token);
+        const canCreateIfStateEmpty = core.getInput('createIfStateEmpty') != 'false';
+        const stateId = core.getInput('assignStoriesFromStateId');
+
+        if (!canCreateIfStateEmpty && await isStateEmpty(client, stateId)) {
+            core.setOutput('iteration-created', false);
+            core.info('No iteration created because the workflow state is empty.');
+            return;
+        }
+
+        const iteration = await createIteration(client);
+
+        assignStoriesToIteration(client, iteration, stateId);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
